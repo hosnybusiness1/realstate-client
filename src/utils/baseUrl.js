c324c75f5fba61f4aba5d4ada20e4ebbbded5575
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2025-08-29 19:03:09 +0300

    Change  live base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "http://localhost:5000/api/v1";
-const BASE_URL = "https://realstate-server-puce.vercel.app/api/v1";
+const BASE_URL = "https://realstate-server-eg.vercel.app/api/v1";
 
 export default BASE_URL;
